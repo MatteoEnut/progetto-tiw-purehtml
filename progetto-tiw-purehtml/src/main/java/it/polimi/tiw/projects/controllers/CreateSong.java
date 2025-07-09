60be079ff65c0bad2b62b5c7baa26c031f23eafc
MatteoEnut <matteo.enut@mail.polimi.it>
2025-07-09 16:33:45 +0200

    added Home

---
+++ progetto-tiw-purehtml/src/main/java/it/polimi/tiw/projects/controllers/CreateSong.java
@@ -0,0 +1,45 @@
+package it.polimi.tiw.projects.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang.StringEscapeUtils;
+
+/**
+ * Servlet implementation class CreateSong
+ */
+@WebServlet("/CreateSong")
+public class CreateSong extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CreateSong() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// If the user is not logged in (not present in session) redirect to the login
+		HttpSession session = request.getSession();
+		if (session.isNew() || session.getAttribute("user") == null) {
+			String loginpath = getServletContext().getContextPath() + "/index.html";
+			response.sendRedirect(loginpath);
+			return;
+		}
+		
+	}
+
+}
